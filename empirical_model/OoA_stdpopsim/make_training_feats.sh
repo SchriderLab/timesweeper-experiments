37a53391780262888cd19e7552f0e40407820334
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-02-15 12:03:20 -0500

    Added script to make training features as well as adjusted some paths.

---
+++ empirical_model/OoA_stdpopsim/make_training_feats.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --partition=general
+#SBATCH --mem=2G
+#SBATCH --ntasks=1
+#SBATCH --time=02:00:00
+#SBATCH -J make_feats
+#SBATCH -o logfiles/make_feats.%A.%a.out
+#SBATCH -e logfiles/make_feats.%A.%a.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=lswhiteh@email.unc.edu
+#SBATCH --array=1-2501:100
+
+conda activate blinx
+
+for i in hard neut soft
+do  
+    for id in $(seq ${SLURM_ARRAY_TASK_ID} $((${SLURM_ARRAY_TASK_ID} + 100)))
+    do
+        indir=sims/vcfs/${i}/${id}
+
+        python /proj/dschridelab/lswhiteh/timesweeper/src/make_training_features.py cli \
+        -s 5 2 1 3 3 3 1 3 4 3 3 3 4 3 4 3 3 4 8 5 \
+        -p 2 \
+        -i $indir/merged.vcf.gz \
+        --sweep $i
+    done
+done
