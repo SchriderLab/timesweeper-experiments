92c80c8f93dab931bd08b487de291055c51c3984
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-03-07 09:37:49 -0500

    Cleaning up and updated slimfile.

--- empirical_model/simple_workflow.sh
+++ empirical_model/simple_workflow.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 #SBATCH --partition=general
-#SBATCH --mem=8G
-#SBATCH -c 5
-#SBATCH --time=4:00:00
-#SBATCH -J classify
-#SBATCH -o logfiles/sims/simulate.%A.%a.out
-#SBATCH -e logfiles/sims/simulate.%A.%a.err
+#SBATCH --mem=64G
+#SBATCH -c 32
+#SBATCH --time=24:00:00
+#SBATCH -J workflow
+#SBATCH -o logfiles/workflow.%A.%a.out
+#SBATCH -e logfiles/workflow.%A.%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=lswhiteh@email.unc.edu
-#SBATCH --array=0-2500:10
+##SBATCH --array=0-2500:10
+conda init bash
 conda activate blinx
+source activate blinx
 
 srcdir=/proj/dschridelab/lswhiteh/timesweeper/src
 configfile=OoA_config.yaml
 
-python ${srcdir}/simulate_stdpopsim.py --rep-range ${SLURM_ARRAY_TASK_ID} $((${SLURM_ARRAY_TASK_ID}+100)) yaml ${configfile}
-#python ${srcdir}/process_vcfs.py yaml ${configfile}
-#python ${srcdir}/make_training_features.py yaml ${configfile}
-#python ${srcdir}/nets.py -n mongolian_samples yaml ${configfile}
+#python ${srcdir}/simulate_stdpopsim.py --rep-range ${SLURM_ARRAY_TASK_ID} $((${SLURM_ARRAY_TASK_ID}+100)) yaml ${configfile}
+python ${srcdir}/process_vcfs.py yaml ${configfile}
+python ${srcdir}/make_training_features.py yaml ${configfile}
+python ${srcdir}/nets.py -n mongolian_samples yaml ${configfile}
