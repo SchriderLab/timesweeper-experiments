a9950a1732a966933658cfd0e0f8b5fd1774da3b
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-03-18 18:12:01 -0400

    More reruns for PR curves and renaming.

--- simple_sims/sel_timing/200_gens/simple_workflow.sh
+++ simple_sims/sel_timing/200_gens/simple_workflow.sh
@@ -18,4 +18,4 @@ configfile=config.yaml
 
 python ${srcdir}/process_vcfs.py yaml ${configfile}
 python ${srcdir}/make_training_features.py yaml ${configfile}
-python ${srcdir}/nets.py -n mongolian_samples yaml ${configfile}
+python ${srcdir}/nets.py -n 200gpost yaml ${configfile}
