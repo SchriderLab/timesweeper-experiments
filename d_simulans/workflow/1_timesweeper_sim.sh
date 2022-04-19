ebca69e2773f5c3ca85dde368f30c898910060f2
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-04-19 14:19:15 -0400

    Genned up some new images for input plots.

--- d_simulans/workflow/1_timesweeper_sim.sh
+++ d_simulans/workflow/1_timesweeper_sim.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #SBATCH --partition=general
-#SBATCH --mem=8G
-#SBATCH -c 4
-#SBATCH --time=2:00:00
+#SBATCH --mem=4G
+#SBATCH -c 2
+#SBATCH --time=8:00:00
 #SBATCH -J workflow
-#SBATCH -o logfiles/workflow.%A.%a.out
-#SBATCH -e logfiles/workflow.%A.%a.err
+#SBATCH -o logfiles/sim.%A.%a.out
+#SBATCH -e logfiles/sim.%A.%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=lswhiteh@email.unc.edu
-#SBATCH --array=0-2500:5
+#SBATCH --array=0-2500
 conda init bash
 conda activate blinx
 source activate blinx
 
 configfile=d_simulans_config.yaml
 cd ..
-python simulate_custom.py --rep-range ${SLURM_ARRAY_TASK_ID} $((${SLURM_ARRAY_TASK_ID}+5)) yaml ${configfile}
+
+python simulate_custom.py --rep-range ${SLURM_ARRAY_TASK_ID} ${SLURM_ARRAY_TASK_ID} yaml ${configfile}
